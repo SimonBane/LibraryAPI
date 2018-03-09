abbeff79495320353e937d99211e2a294cfc67b4
SimonBane <simeon.banev3@gmail.com>
2018-03-09 08:22:35 +0200

    Started IdentityServer authorization.

---
+++ AuthorizationServer/Configuration/Config.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthorizationServer.Configuration
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("libraryAPI", "LibraryAPI")
+                {
+                    ApiSecrets = {new Secret("library".Sha256())},
+                },
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new Client[]
+            {
+                new Client()
+                {
+                    ClientId = "library-api",
+                    ClientName = "LibraryAPI",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+                    RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
+                    EnableLocalLogin = true,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "library_api"
+                    },
+                    ClientSecrets = new List<Secret>(){new Secret("library".Sha256())}
+                },
+            };
+        }
+    }
+}
---
+++ AuthorizationServer/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        public static IConfigurationRoot ConfigurationRoot;
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            ConfigurationRoot = builder.Build();
+
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("Home/Error");
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
